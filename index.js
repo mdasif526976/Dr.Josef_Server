cc439a5f78f5d6b482c9195339480499b0d9f871
mdasif526976 <mdasif526976@gmail.com>
2022-11-10 11:20:46 +0600

    cors install

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.Port || 5000;
 
 // middle wire
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res)=>{
      res.send('all is ok')
