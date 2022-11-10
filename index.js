88b8ee5a4ee8336e4377973409d7a4e4e47b2682
mdasif526976 <mdasif526976@gmail.com>
2022-11-10 19:05:37 +0600

    .env set up

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.Port || 5000;
 
 // middle wire
 app.use(cors());
 app.use(express.json());
+
+// start crud oparation
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@atlascluster.ul3fosw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 app.get('/', (req, res)=>{
      res.send('all is ok')
